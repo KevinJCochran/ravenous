810f564440dfcee6b76094e2a875d8ec869322bc
kjc199 <appleid.kjc@gmail.com>
2019-04-11 18:48:17 -0400

    Updates

---
+++ precache-manifest.24cf2a0053b2548b1646d2c40a9b890a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "e75fbcf8c645c78e51ca",
+    "url": "/ravenous/static/js/main.95d1cd31.chunk.js"
+  },
+  {
+    "revision": "3f59b0efc0fe4a51b374",
+    "url": "/ravenous/static/js/2.5960d69b.chunk.js"
+  },
+  {
+    "revision": "e75fbcf8c645c78e51ca",
+    "url": "/ravenous/static/css/main.959f44f4.chunk.css"
+  },
+  {
+    "revision": "daa70860a7a41b449619495dd91193b4",
+    "url": "/ravenous/index.html"
+  }
+];
